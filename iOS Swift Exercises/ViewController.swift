e11ccfb33fc53c024a758ea00b277ef72c07ad00
Allen Pan <allen@quizlet.com>
2019-07-23 11:59:31 -0700

    add fake table view with some example cells

--- iOS Swift Exercises/ViewController.swift
+++ iOS Swift Exercises/ViewController.swift
@@ -1,12 +1,30 @@
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    private lazy var tableView: UITableView = { [unowned self] in
+        let tableView = UITableView(frame: .zero)
+        tableView.dataSource = self
+        tableView.allowsSelection = false
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.addSubview(tableView)
+        tableView.frame = self.view.bounds
+
+        tableView.reloadData()
     }
 
+    // MARK: - UITableViewDataSource implementation
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Test Cell"
+        return cell
+    }
 }
-
